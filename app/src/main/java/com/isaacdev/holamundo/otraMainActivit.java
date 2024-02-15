08b58a1cf516851939735a800056126cc8c57e34
isaac <isaacmartinez53789@gmail.com>
2024-02-15 09:23:03 -0600

    carga inicial del proyecto android.

---
+++ app/src/main/java/com/isaacdev/holamundo/otraMainActivit.java
@@ -0,0 +1,18 @@
+package com.isaacdev.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class otraMainActivit extends AppCompatActivity {
+
+    Button btnOtra;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otra_main);
+    }
+}
